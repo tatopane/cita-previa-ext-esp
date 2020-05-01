16ab740555e004adc773eb7188131853461221b4
Tato Panelati <panelati@dhcp-18-20-138-113.dyn.mit.edu>
2020-05-01 16:23:13 -0400

    use chrome local storage instead of variables

--- scripts/background.js
+++ scripts/background.js
@@ -1,48 +1,57 @@
-
-var toggle = false;
-var status = 'off';
-var the_tab_id = '';
-
-function set_status() { // FIXME: use chrome storage for property: https://stackoverflow.com/questions/32684346/chrome-extension-not-saving-data
-    toggle = !toggle;
-    status = 'off';
-    if(toggle) { status = 'on';}
+function setData(data){
+    chrome.storage.local.set(data, function(){
+        if(!chrome.runtime.lastError){
+        console.log('Saved: ', data);
+        }   
+    });
 }
 
-function toggle_extension(the_tabid){
-    
-    // console.log(the_tabid, "inside", status);
-    // Set icon
+function toggle_extension(status, tab_id){
     chrome.browserAction.setIcon({ path: { "19": "images/delivery-"+status+"-19.png",
-                                       "38": "images/delivery-"+status+"-38.png" }, tabId: the_tabid});
+                                           "38": "images/delivery-"+status+"-38.png" }, tabId: tab_id});
     // Pass variable & execute script
-    chrome.tabs.executeScript((the_tabid,  { code: 'var extension_status = "'+status+'"' }));
-    console.log("status "+status);
+    chrome.tabs.executeScript((tab_id,  { code: 'var extension_status = "'+status+'"' }));
     if (status == 'on') {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		  chrome.tabs.sendMessage(the_tabid, {message: "search_delivery"}, function(response) {
-		    console.log("message sent");
-		  });
-		});
+          chrome.tabs.sendMessage(tab_id, {message: "search_delivery"}, function(response) {
+            console.log("message sent");
+          });
+        });
     }
-    
 }
 
 function my_listener(tabId, changeInfo, tab) {
-	// console.log(tabId, tabId==the_tab_id);
-	// console.log(changeInfo, changeInfo.status=="complete", status);
-	console.log("status in listener "+status);
-    // If updated tab matches this one
-    if (tabId == the_tab_id && changeInfo.status == "complete" && status == 'on') {
-        toggle_extension(the_tab_id);
-    }
+	chrome.storage.local.get(['toggle','status','the_tab_id'], function(result) {
+        if (result){
+            data = result;
+            // If updated tab matches this one
+            if (tabId == data.the_tab_id && changeInfo.status == "complete" && data.status == 'on') {
+                toggle_extension(data.status, data.the_tab_id);
+            }
+        }
+    });
+    
 }
 
+var dataObj = {};
+dataObj["toggle"] = false;
+dataObj["status"] = "off";
+dataObj["the_tab_id"] = "";
+setData(dataObj);
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-    set_status();
-    // Set the tab id
-    the_tab_id = tab.id;
-    toggle_extension(the_tab_id);
+    chrome.storage.local.get(['toggle','status','the_tab_id'], function(result) {
+        if (result){
+            data = result;
+            console.log("Read:", data);
+            data["toggle"] = !data["toggle"];
+            data["status"] = 'off';
+            data["the_tab_id"] = tab.id;
+            if(data["toggle"]) { data["status"] = 'on';}
+            setData(data);
+            toggle_extension(data["status"], tab.id);
+        }
+    });
 });
 
 chrome.tabs.onUpdated.addListener(my_listener);
